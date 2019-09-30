3adab2939bc2416177064eae8f5718c6602ff930
RahmaBasly <35639830+RahmaBasly@users.noreply.github.com>
2019-09-30 22:06:17 +0100

    Creation  du composant : - page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AdzRechercheComponent} from '../app/adz-recherche/adz-recherche.component';
 import {AdzResultatComponent} from '../app/adz-resultat/adz-resultat.component';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  {path: '', component:AdzRechercheComponent },
-  {path: 'resultat/:q', component:AdzResultatComponent }
+  {path: 'recherche', component:AdzRechercheComponent },
+  {path: 'resultat/:q', component:AdzResultatComponent },
+  { path: '',   redirectTo: '/recherche', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
